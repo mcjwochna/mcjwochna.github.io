3a0db46261fff3b7f00383539567dd21365a0cc9
Maciej Wochna <maciej.wochna@gmail.com>
2026-01-14 11:24:53 +0100

    Fix: Add missing deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  // Zastąp 'twoj-nick' swoim prawdziwym nickiem z GitHuba
-  site: 'https://maciejwochna.github.io', 
+  site: 'https://mcjwochna.github.io', 
   integrations: [tailwind()],
 });
